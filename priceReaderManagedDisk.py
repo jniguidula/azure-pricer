19f2acb94752673d3533b647dc72b3f80ff2860a
jniguidula <38459441+jniguidula@users.noreply.github.com>
2020-10-22 11:13:18 +0200

    Update priceReaderManagedDisk.py
    added addtional managed disks - pricer will not size correctly when data disk >4096GB

--- priceReaderManagedDisk.py
+++ priceReaderManagedDisk.py
@@ -3,8 +3,8 @@ import json
 
 urlPriceManagedDiskPublicAPI='https://azure.microsoft.com/api/v2/pricing/managed-disks/calculator/?culture=en-us&discount=mosp&currency=eur'
 
-premiumDiskSizes  = ['P4', 'P6', 'P10', 'P15', 'P20', 'P30', 'P40', 'P50']
-standardDiskSizes = ['S4', 'S6', 'S10', 'S15', 'S20', 'S30', 'S40', 'S50']
+premiumDiskSizes  = ['P4', 'P6', 'P10', 'P15', 'P20', 'P30', 'P40', 'P50', 'P60', 'P70', 'P80']
+standardDiskSizes = ['S4', 'S6', 'S10', 'S15', 'S20', 'S30', 'S40', 'S50', 'S60', 'S70', 'S80']
 
 def getPriceMatrixStandard(regions):
 	with urllib.request.urlopen(urlPriceManagedDiskPublicAPI) as url:
